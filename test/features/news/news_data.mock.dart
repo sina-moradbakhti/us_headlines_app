dadcad303275ec822609991a2f6dfb7d14baf461
Sina Moradbakhti <sina.moradbakhti@gmail.com>
2024-07-12 15:19:01 +0330

    new update on ordering the list - updated the controller - updated the test scenario - new test case - updated strings file

--- test/features/news/news_data.mock.dart
+++ test/features/news/news_data.mock.dart
@@ -1,5 +1,6 @@
 import 'package:nexttoptech_assignment/core/data/models/articles_response_model.dart';
 import 'package:nexttoptech_assignment/core/data/models/source_model.dart';
+import 'package:nexttoptech_assignment/core/utils/enums.dart';
 import 'package:nexttoptech_assignment/features/news/data/models/article_model.dart';
 
 class NewsDataMock {
@@ -13,6 +14,7 @@ class NewsDataMock {
           id: '0',
           name: 'source_name',
         ),
+        company: FanCompany.microsoft,
       ),
       ArticleModel(
         title: "article 2",
@@ -20,6 +22,7 @@ class NewsDataMock {
           id: '0',
           name: 'source_name',
         ),
+        company: FanCompany.apple,
       ),
       ArticleModel(
         title: "article 3",
@@ -27,6 +30,15 @@ class NewsDataMock {
           id: '0',
           name: 'source_name',
         ),
+        company: FanCompany.google,
+      ),
+      ArticleModel(
+        title: "article 4",
+        source: SourceModel(
+          id: '0',
+          name: 'source_name',
+        ),
+        company: FanCompany.tesla,
       ),
     ],
   );
@@ -36,11 +48,20 @@ class NewsDataMock {
     status: "ok",
     articles: [
       ArticleModel(
+        title: "article 4",
+        source: SourceModel(
+          id: '0',
+          name: 'source_name',
+        ),
+        company: FanCompany.microsoft,
+      ),
+      ArticleModel(
         title: "article 3",
         source: SourceModel(
           id: '0',
           name: 'source_name',
         ),
+        company: FanCompany.apple,
       ),
       ArticleModel(
         title: "article 2",
@@ -48,6 +69,7 @@ class NewsDataMock {
           id: '0',
           name: 'source_name',
         ),
+        company: FanCompany.google,
       ),
       ArticleModel(
         title: "article 1",
@@ -55,6 +77,7 @@ class NewsDataMock {
           id: '0',
           name: 'source_name',
         ),
+        company: FanCompany.tesla,
       ),
     ],
   );
@@ -72,6 +95,7 @@ class NewsDataMock {
         id: '0',
         name: 'source_name',
       ),
+      company: FanCompany.microsoft,
     ),
     ArticleModel(
       title: "article from cache 2",
@@ -79,6 +103,7 @@ class NewsDataMock {
         id: '0',
         name: 'source_name',
       ),
+      company: FanCompany.apple,
     ),
     ArticleModel(
       title: "article from cache 3",
@@ -86,6 +111,15 @@ class NewsDataMock {
         id: '0',
         name: 'source_name',
       ),
+      company: FanCompany.google,
+    ),
+    ArticleModel(
+      title: "article from cache 4",
+      source: SourceModel(
+        id: '0',
+        name: 'source_name',
+      ),
+      company: FanCompany.tesla,
     ),
   ];
 }
--- test/features/news/test.dart
+++ test/features/news/test.dart
@@ -22,7 +22,6 @@ void main() {
     test('Initial values are correct', () {
       expect(newsController.currentPage.value, 1);
       expect(newsController.sortBy.value, SortBy.publishedAt);
-      expect(newsController.company.value, FanCompany.microsoft);
       expect(newsController.hasNetworkConnection.value, true);
       expect(newsController.articles, isEmpty);
       expect(newsController.cachedArticles, isEmpty);
@@ -31,15 +30,29 @@ void main() {
     test('Fetches latest news on init with network connection', () async {
       await newsController.init();
 
-      expect(newsController.articles.length, 3);
+      expect(newsController.articles.length, 16);
       expect(newsController.articles[0].title, "article 1");
+    });
+
+    test(
+        'Latest news must be in order like this, Microsoft, Apple, Google, Tesla and so on',
+        () async {
+      await newsController.init();
+
+      expect(newsController.articles.length, 16);
+      expect(newsController.articles[0].company, FanCompany.microsoft);
+      expect(newsController.articles[1].company, FanCompany.apple);
+      expect(newsController.articles[2].company, FanCompany.google);
+      expect(newsController.articles[3].company, FanCompany.tesla);
+      expect(newsController.articles[4].company, FanCompany.microsoft);
+      expect(newsController.articles[5].company, FanCompany.apple);
     });
 
     test('Loads cached news on init without network connection', () async {
       newsController.hasNetworkConnection.value = false;
       await newsController.init(bypassInternetCheck: true);
 
-      expect(newsController.cachedArticles.length, 3);
+      expect(newsController.cachedArticles.length, 4);
       expect(newsController.cachedArticles[0].title, "article from cache 1");
       expect(newsController.articles.length, 0);
     });
@@ -56,20 +69,6 @@ void main() {
       await Future.delayed(const Duration(seconds: 1));
 
       expect(sortByListenerTriggered, isTrue);
-    });
-
-    test('Fetch new data after chnaging Company', () async {
-      bool fanCompanyListenerTriggered = false;
-
-      ever(newsController.company, (FanCompany company) {
-        fanCompanyListenerTriggered = true;
-      });
-
-      newsController.company.value = FanCompany.tesla;
-
-      await Future.delayed(const Duration(seconds: 1));
-
-      expect(fanCompanyListenerTriggered, isTrue);
     });
   });
 }
